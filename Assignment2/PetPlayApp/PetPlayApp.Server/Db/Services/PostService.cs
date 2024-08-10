c146bac8aa5eaa9746b3a43feb798591557e6057
Kyan Grimm <kyan.grimm@Wisetechglobal.com>
2024-08-10 12:18:45 +1000

    Added db services

---
+++ Assignment2/PetPlayApp/PetPlayApp.Server/Db/Services/PostService.cs
@@ -0,0 +1,14 @@
+﻿using PetPlayApp.Server.Db.Repos;
+
+namespace PetPlayApp.Server.Db.Services
+{
+    public class PostService
+    {
+        private readonly PostRepository _postRepo;
+
+        public PostService(PostRepository postRepo)
+        {
+            _postRepo = postRepo;
+        }
+    }
+}
---
+++ Assignment2/PetPlayApp/PetPlayApp.Server/Db/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using PetPlayApp.Server.Db.Repos;
+
+namespace PetPlayApp.Server.Db.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepo;
+
+        public UserService(UserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+    }
+}
