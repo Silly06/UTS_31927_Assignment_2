f1f38aca5af88539e641f6979446625f88c14078
Aidan Lei <aidan.lei@wisetechglobal.com>
2024-08-10 12:22:26 +1000

    Fix frontend

--- Assignment2/PetPlayApp/petplayapp.client/src/main.ts
+++ Assignment2/PetPlayApp/petplayapp.client/src/main.ts
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import HomeVue from './components/Home.vue'
+
+const routes = [
+    { path: '/', component: HomeVue },
+];
 
 createApp(App).mount('#app')
