6d3f3994ec896cc162dc1d770497422e11b5ee47
Aidan Lei <aidan.lei@wisetechglobal.com>
2024-08-10 14:28:10 +1000

    fix

--- Assignment2/PetPlayApp/petplayapp.client/src/components/Home.vue
+++ Assignment2/PetPlayApp/petplayapp.client/src/components/Home.vue
@@ -6,16 +6,4 @@
 </template>
 
 <script setup lang="ts">
-import axios from 'axios';
-
-const getPosts = async () => {
-  try {
-    const response = await axios.get('/home/getposts');
-    // Handle the response data here
-  } catch (error) {
-    // Handle the error here
-  }
-};
-
-getPosts();
 </script>
