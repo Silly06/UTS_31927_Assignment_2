588476949e0fa25bf076d7047402c7ae0c88cae7
Bhryno <bryanrplee@gmail.com>
2024-08-18 09:02:08 +1000

    Migrate stories

---
+++ Assignment2/PetPlayApp/PetPlayApp.Server/Migrations/20240817125444_Stories.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetPlayApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Stories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
